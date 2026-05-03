8f854b1bedd8952e06e2be7e2cb11adcc3a46ea9
Roonyexx <roonyexx@gmail.com>
2026-05-03 19:30:08 +0700

    rewrite scaner it now based on state machine logic

--- PlSqlMergeTool.BLL/LexicalAnalysis/Scaner.cs
+++ PlSqlMergeTool.BLL/LexicalAnalysis/Scaner.cs
@@ -1,79 +1,144 @@
+using System;
 using PlSqlMergeTool.BLL.Models;
 
 namespace PlSqlMergeTool.BLL.LexicalAnalysis;
 
-public class Scaner(string input)
+public class Scanner(string input)
 {
     private readonly string _input = input;
     private int _position = 0;
     private int _currentLine = 1;
+    private static readonly string[] ComplexOperators = 
+    [
+        ":=", ">=", "<=", "!=", "<>", "^=", "~=", "||", "**", "=>", "<<", ">>"
+    ];
 
-    private void SkipWhitespace()
-    {
-        while (_position < _input.Length && char.IsWhiteSpace(_input[_position]))
-        {
-            if (_input[_position] == '\n') _currentLine++;
-            _position++;
-        }
-    }
     public PlSqlToken? GetNextToken()
     {
-        SkipWhitespace();
         if (_position >= _input.Length) return null;
 
         int startOffset = _position;
-
-        if(char.IsLetter(_input[_position]) || _input[_position] == '_')
+        int startLine = _currentLine;
+        char currentChar = _input[_position];
+        // пробелы и табы, переносы строк
+        if (char.IsWhiteSpace(currentChar))
         {
-            while (_position < _input.Length && (char.IsLetterOrDigit(_input[_position]) || _input[_position] == '_'))
+            while (_position < _input.Length && char.IsWhiteSpace(_input[_position]))
+            {
+                if (_input[_position] == '\n') _currentLine++;
+                _position++;
+            }
+            return CreateToken(startOffset, startLine, TokenType.Whitespace);
+        }
+        // однострочные комментарии
+        if (currentChar == '-' && _position + 1 < _input.Length && _input[_position + 1] == '-')
+        {
+            _position += 2;
+            while (_position < _input.Length && _input[_position] != '\n')
             {
                 _position++;
             }
+            return CreateToken(startOffset, startLine, TokenType.SingleLineComment);
         }
-        else if (_input[_position] == '-')
+        // многострочные комментарии
+        if (currentChar == '/' && _position + 1 < _input.Length && _input[_position + 1] == '*')
+        {
+            _position += 2;
+            while (_position < _input.Length)
+            {
+                if (_input[_position] == '*' && _position + 1 < _input.Length && _input[_position + 1] == '/')
+                {
+                    _position += 2;
+                    break;
+                }
+                if (_input[_position] == '\n') _currentLine++;
+                _position++;
+            }
+            return CreateToken(startOffset, startLine, TokenType.MultiLineComment);
+        }
+        // строки
+        if (currentChar == '\'')
         {
             _position++;
-            if (_position < _input.Length && _input[_position] == '-')
+            while (_position < _input.Length)
             {
-                while (_position < _input.Length && _input[_position] != '\n')
+                if (_input[_position] == '\'')
                 {
                     _position++;
+                    if (_position < _input.Length && _input[_position] == '\'')
+                        _position++;
+                    else
+                        break; 
                 }
-            }
-        }
-        else if (_input[_position] == '/')
-        {
-            _position++;
-            if (_position < _input.Length && _input[_position] == '*')
-            {
-                _position++;
-                while (_position < _input.Length - 1)
+                else
                 {
-                    if (_input[_position] == '*' && _input[_position + 1] == '/')
-                    {
-                        _position += 2;
-                        break;
-                    }
                     if (_input[_position] == '\n') _currentLine++;
                     _position++;
                 }
-                if (_position == _input.Length - 1) _position++; 
+            }
+            return CreateToken(startOffset, startLine, TokenType.String);
+        }
+
+        // слова и идентификаторы
+        if (char.IsLetter(currentChar) || currentChar == '_')
+        {
+            while (_position < _input.Length && 
+                  (char.IsLetterOrDigit(_input[_position]) || _input[_position] == '_' || _input[_position] == '$' || _input[_position] == '#'))
+            {
+                _position++;
+            }
+            return CreateToken(startOffset, startLine, TokenType.Word);
+        }
+
+        // числа
+        if (char.IsDigit(currentChar))
+        {
+            while (_position < _input.Length && (char.IsDigit(_input[_position]) || _input[_position] == '.'))
+            {
+                _position++;
+            }
+            return CreateToken(startOffset, startLine, TokenType.Number);
+        }
+
+        // операторы из нескольких символов
+        var remainingSpan = _input.AsSpan(_position);
+        foreach (var op in ComplexOperators)
+        {
+            if (remainingSpan.StartsWith(op))
+            {
+                _position += op.Length;
+                return CreateToken(startOffset, startLine, TokenType.Operator);
             }
         }
-        else
+
+        // разделители и одиночные операторы
+        if ("();,.".Contains(currentChar))
         {
             _position++;
+            return CreateToken(startOffset, startLine, TokenType.Delimiter);
         }
 
-        int tokenLength = _position - startOffset;
-        string token = _input[startOffset.._position];
+        if ("+-*/=<>:@%".Contains(currentChar))
+        {
+            _position++;
+            return CreateToken(startOffset, startLine, TokenType.Operator);
+        }
 
+        // фоллбек для неизвестных символов
+        _position++;
+        return CreateToken(startOffset, startLine, TokenType.Unknown);
+    }
+
+    private PlSqlToken CreateToken(int startOffset, int startLine, TokenType type)
+    {
+        int length = _position - startOffset;
         return new PlSqlToken
         {
-            Text = token,
-            Line = _currentLine,
+            Text = _input.Substring(startOffset, length),
+            Line = startLine,
             Offset = startOffset,
-            Length = tokenLength
+            Length = length,
+            Type = type
         };
     }
 }
