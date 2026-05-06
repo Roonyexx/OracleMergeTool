6bc6ccc1754c32013c7ff324b1e3dfeb9497adbd
Roonyexx <roonyexx@gmail.com>
2026-05-06 14:58:51 +0700

    added MergeRuleBuilder class that collects chain of merge rule

---
+++ PlSqlMergeTool.BLL/Services/MergeRuleBuilder.cs
@@ -0,0 +1,54 @@
+using PlSqlMergeTool.BLL.MergeLogic;
+
+namespace PlSqlMergeTool.BLL.Services;
+
+public class MergeRuleBuilder
+{
+    private readonly List<MergeRuleHandler> _rules = [];
+
+    public MergeRuleBuilder AddNoChangesRule()
+    {
+        _rules.Add(new NoChangesRule());
+        return this;
+    }
+
+    public MergeRuleBuilder AddVendorModificationRule()
+    {
+        _rules.Add(new VendorModificationRule());
+        return this;
+    }
+
+    public MergeRuleBuilder AddBankModificationRule()
+    {
+        _rules.Add(new BankModificationRule());
+        return this;
+    }
+
+    public MergeRuleBuilder AddConflictRule()
+    {
+        _rules.Add(new MergeConflictRule());
+        return this;
+    }
+
+    public MergeRuleBuilder AddCustomRule(MergeRuleHandler customRule)
+    {
+        _rules.Add(customRule);
+        return this;
+    }
+
+    public MergeRuleHandler Build()
+    {
+        if (_rules.Count == 0)
+        {
+            throw new InvalidOperationException(
+                "No rules configured. Add at least one rule before building the chain.");
+        }
+
+        for (int i = 0; i < _rules.Count - 1; i++)
+        {
+            _rules[i].SetNext(_rules[i + 1]);
+        }
+
+        return _rules[0];
+    }
+}
