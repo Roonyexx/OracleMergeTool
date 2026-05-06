9f06f0c55ef5035fda8946b707caf5f84d91f586
Roonyexx <roonyexx@gmail.com>
2026-05-06 13:07:12 +0700

    created TokenChanker

---
+++ PlSqlMergeTool.BLL/LexicalAnalysis/TokenChunker.cs
@@ -0,0 +1,21 @@
+using System;
+using DiffPlex;
+using DiffPlex.Chunkers;
+using PlSqlMergeTool.BLL.Services;
+
+namespace PlSqlMergeTool.BLL.LexicalAnalysis;
+
+public class TokenChunker(SqlAnalyserService sqlAnalyserService) : IChunker
+{
+    private readonly SqlAnalyserService _sqlAnalyserService = sqlAnalyserService;
+    public string[] Chunk(string text)
+    {
+        var parsedDoc = _sqlAnalyserService.Tokenize(text);
+        return [.. parsedDoc.CleanTokens.Select(t => t.Text)];
+    }
+
+    IReadOnlyList<string> IChunker.Chunk(string text)
+    {
+        return Chunk(text);
+    }
+}
