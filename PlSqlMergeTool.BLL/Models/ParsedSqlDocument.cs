8da08ea3cb4bc41cf608292dd6ee8477ec219a47
Roonyexx <roonyexx@gmail.com>
2026-05-06 14:30:38 +0700

    ParsedSqlDocument now has a FileName

--- PlSqlMergeTool.BLL/Models/ParsedSqlDocument.cs
+++ PlSqlMergeTool.BLL/Models/ParsedSqlDocument.cs
@@ -2,6 +2,7 @@ namespace PlSqlMergeTool.BLL.Models;
 
 public class ParsedSqlDocument
 {
+    public string FileName { get; set; } = string.Empty;
     public string OriginalText { get; set; } = string.Empty;
     public List<PlSqlToken> RawTokens { get; set; } = [];
     public List<PlSqlToken> CleanTokens { get; set; } = [];
