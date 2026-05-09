e8761ba23a89498ddfb6cacd8ce8fff5d1322123
Roonyexx <roonyexx@gmail.com>
2026-05-09 12:56:57 +0700

    implement oracle repository

---
+++ PlSqlMergeTool.DAL/Repositories/OraclePackageRepository.cs
@@ -0,0 +1,81 @@
+using System.Text;
+using Dapper;
+using Oracle.ManagedDataAccess.Client;
+using PlSqlMergeTool.BLL.Interfaces;
+using PlSqlMergeTool.BLL.Models;
+
+namespace PlSqlMergeTool.DAL.Repositories;
+
+public class OracleRepository : IOracleRepository // todo разделить поведение на разные интерфейсы
+{
+    public void CompilePackage(string сonnectionString, string resolvedSqlText)
+    {
+        using var connection = new OracleConnection(сonnectionString);
+        connection.Execute(resolvedSqlText);
+    }
+
+    public IEnumerable<OracleCompileError> GetCompilationErrors(string сonnectionString, string packageName)
+    {
+        using var connection = new OracleConnection(сonnectionString);
+        
+        const string sql = @"
+            SELECT LINE as Line, 
+                   POSITION as Position, 
+                   TEXT as ErrorText 
+            FROM ALL_ERRORS 
+            WHERE NAME = :PackageName 
+              AND TYPE IN ('PACKAGE', 'PACKAGE BODY')
+            ORDER BY TYPE, SEQUENCE";
+
+        return connection.Query<OracleCompileError>(sql, new { PackageName = packageName });
+    }
+
+    public IEnumerable<string> GetPackageNames(string connectionString)
+    {
+        using var connection = new OracleConnection(connectionString);
+        
+        const string sql = @"
+            SELECT DISTINCT NAME 
+            FROM ALL_SOURCE 
+            WHERE TYPE IN ('PACKAGE', 'PACKAGE BODY')
+            ORDER BY NAME";
+            
+        return connection.Query<string>(sql);
+    }
+
+    public string GetPackageSource(string connectionString, string packageName)
+    {
+        using var connection = new OracleConnection(connectionString);
+        
+        const string sql = @"
+            SELECT TEXT 
+            FROM ALL_SOURCE 
+            WHERE NAME = :PackageName 
+              AND TYPE IN ('PACKAGE', 'PACKAGE BODY')
+            ORDER BY TYPE, LINE"; 
+
+        var lines = connection.Query<string>(sql, new { PackageName = packageName });
+        
+        var sb = new StringBuilder();
+        foreach (var line in lines)
+        {
+            sb.Append(line);
+        }
+        
+        return sb.ToString();
+    }
+
+    public IEnumerable<TableColumnMetadata> GetSchemaTablesMetadata(string connectionString)
+    {
+        using var connection = new OracleConnection(connectionString);
+        
+        const string sql = @"
+            SELECT TABLE_NAME as TableName, 
+                   COLUMN_NAME as ColumnName, 
+                   DATA_TYPE as DataType 
+            FROM USER_TAB_COLUMNS
+            ORDER BY TABLE_NAME, COLUMN_ID";
+
+        return connection.Query<TableColumnMetadata>(sql);
+    }
+}
