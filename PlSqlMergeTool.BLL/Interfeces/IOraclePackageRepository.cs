afa3e7e4294c3f8e979cbd530a25cfe937a02eac
Roonyexx <roonyexx@gmail.com>
2026-05-09 11:48:07 +0700

    added behavior to interface

--- PlSqlMergeTool.BLL/Interfeces/IOraclePackageRepository.cs
+++ PlSqlMergeTool.BLL/Interfeces/IOraclePackageRepository.cs
@@ -1,12 +1,19 @@
 using System.Collections.Generic;
+using PlSqlMergeTool.BLL.Models;
 
 namespace PlSqlMergeTool.BLL.Interfaces;
 
-public interface IOraclePackageRepository
+public interface IOracleRepository
 {
     // получить список всех пакетов в схеме
     IEnumerable<string> GetPackageNames(string connectionString);
 
     // извлечь исходный код конкретного пакета
     string GetPackageSource(string connectionString, string packageName);
+
+    IEnumerable<TableColumnMetadata> GetSchemaTablesMetadata(string connectionString);
+
+    void CompilePackage(string targetConnectionString, string resolvedSqlText);
+
+    IEnumerable<OracleCompileError> GetCompilationErrors(string targetConnectionString, string packageName);
 }
